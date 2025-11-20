5dae2df2630ba49d8cb645c98bee0e6d56648ee1
LACT17 <alfonsolact10@gmail.com>
2025-11-20 16:03:32 -0700

    Added kafka message

---
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/publisher/kafka/CreateOrderKafkaMessagePublisher.java
@@ -0,0 +1,31 @@
+package com.food.ordering.system.order.service.messaging.publisher.kafka;
+
+import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
+import com.food.ordering.system.kafka.producer.service.KafkaProducer;
+import com.food.ordering.system.order.service.domain.config.OrderServiceConfigData;
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment.OrderCreatedPaymentRequestMessagePublisher;
+import com.food.ordering.system.order.service.messaging.mapper.OrderMessagingDataMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CreateOrderKafkaMessagePublisher implements OrderCreatedPaymentRequestMessagePublisher {
+
+    private final OrderMessagingDataMapper orderMessagingDataMapper;
+    private final OrderServiceConfigData orderServiceConfigData;
+    private final KafkaProducer<String, PaymentRequestAvroModel> kafkaProducer;
+
+    public CreateOrderKafkaMessagePublisher(OrderMessagingDataMapper orderMessagingDataMapper, OrderServiceConfigData orderServiceConfigData, KafkaProducer<String, PaymentRequestAvroModel> kafkaProducer) {
+        this.orderMessagingDataMapper = orderMessagingDataMapper;
+        this.orderServiceConfigData = orderServiceConfigData;
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @Override
+    public void publish(OrderCreatedEvent domainEvent){
+
+    }
+
+}
